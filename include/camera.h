09eb53a4d47812d540f0cec8a0ee7190fa900686
abhijit424515 <abhijit.amrendra.kumar@gmail.com>
2024-03-30 13:49:52 +0530

    metal and dielectric materials added

--- include/camera.h
+++ include/camera.h
@@ -2,7 +2,7 @@
 #define CAMERA_H
 
 #include "color.h"
-#include "hittable.h"
+#include "material.h"
 
 class camera {
 public:
@@ -68,8 +68,11 @@ private:
 
     hit_record rec;
     if (world.hit(r, interval(epsilon, infinity), rec)) {
-      vec3 direction = rec.normal + random_unit_vector();
-      return 0.5 * ray_color(ray(rec.p, direction), depth - 1, world);
+      ray scattered;
+      color attenuation;
+      if (rec.mat->scatter(r, rec, attenuation, scattered))
+        return attenuation * ray_color(scattered, depth - 1, world);
+      return color(0, 0, 0);
     }
 
     vec3 unit_direction = unit_vector(r.direction());
--- include/hittable.h
+++ include/hittable.h
@@ -4,9 +4,12 @@
 #include "interval.h"
 #include "ray.h"
 
+class material;
+
 struct hit_record {
   point3 p;
   vec3 normal;
+  shared_ptr<material> mat;
   double t;
   bool front_face;
 
